ae1d62baa29fddc57d9a818c749b6e3b2057caa2
rizaldi <rizal.gamait@gmail.com>
2026-09-23 02:26:03 +0700

    Use native LARAVEL_STORAGE_PATH and errorlog channel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (env('APP_ENV') === 'production') {
-            $this->app->useStoragePath('/tmp');
-        }
+        //
     }
 
     /**
