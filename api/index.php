ad669215c6941327ffa31aac56d3ea96af49502a
rizaldi <rizal.gamait@gmail.com>
2026-09-23 02:23:51 +0700

    Add error reporting to api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Fatal Error</h1>";
+    echo "<pre>" . $e->getMessage() . "</pre>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
